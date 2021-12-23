cd6f71785780d907a45ad7e04299e5d5b44b6b71
Abyssus1010 <wilson1010x@gmail.com>
2021-12-23 19:36:20 +0800

    dev-thingsboard-agent: modify ThingsBoardInputAgentLauncher to utilize the JPS Agent framework

--- Agents/ThingsBoardAgent/ThingsBoardInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/thingsboard/ThingsBoardInputAgentLauncher.java
+++ Agents/ThingsBoardAgent/ThingsBoardInputAgent/src/main/java/uk/ac/cam/cares/jps/agent/thingsboard/ThingsBoardInputAgentLauncher.java
@@ -1,22 +1,34 @@
 package uk.ac.cam.cares.jps.agent.thingsboard;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.jps.base.timeseries.TimeSeriesClient;
+import uk.ac.cam.cares.jps.base.agent.JPSAgent;
 
 import java.io.IOException;
 import java.time.OffsetDateTime;
-
+import javax.servlet.annotation.WebServlet;
+import javax.ws.rs.BadRequestException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Class with a main method that is the entry point of the compiled jar and puts all components together to retrieve
+ * Class with a main method that is the entry point of the compiled war and puts all components together to retrieve
  * data from the API and write it into the database.
  * @author 
  */
-public class ThingsBoardInputAgentLauncher {
-
+@WebServlet(urlPatterns = {"/retrieve"})
+public class ThingsBoardInputAgentLauncher extends JPSAgent {
+	
+	public static final String KEY_AGENTPROPERTIES = "agentProperties";
+	public static final String KEY_APIPROPERTIES = "apiProperties";
+	public static final String KEY_CLIENTPROPERTIES = "clientProperties";
+	
+	
+	 String agentProperties;
+	 String apiProperties;
+	 String clientProperties;
     /**
      * Logger for reporting info/errors.
      */
@@ -31,14 +43,64 @@ public class ThingsBoardInputAgentLauncher {
     private static final String CONNECTOR_ERROR_MSG = "Could not construct the ThingsBoard API connector needed to interact with the API!";
     private static final String GET_READINGS_ERROR_MSG = "Some readings could not be retrieved.";
 
-    // TODO: Use proper argument parsing
+    
+    @Override
+    public JSONObject processRequestParameters(JSONObject requestParams) {
+      if (validateInput(requestParams)) {
+        	LOGGER.info("Passing request to ThingsBoard Input Agent..");
+            String agentProperties = System.getenv(requestParams.getString(KEY_AGENTPROPERTIES));
+            String clientProperties = System.getenv(requestParams.getString(KEY_CLIENTPROPERTIES));
+            String apiProperties = System.getenv(requestParams.getString(KEY_APIPROPERTIES));
+            String[] args = new String[] {agentProperties,clientProperties,apiProperties};
+            initializeAgent(args);
+            }
+        JSONObject jsonMessage = new JSONObject();
+        jsonMessage.put("Result", "Timeseries Data has been updated.");
+        requestParams = jsonMessage;
+	return requestParams;
+}
+    
+    @Override
+    public boolean validateInput(JSONObject requestParams) throws BadRequestException {
+      boolean validate = true;
+      String agentProperties;
+      String apiProperties;
+      String clientProperties;
+      if (!requestParams.isEmpty()) {
+    	 try {
+    		 agentProperties = (requestParams.getString(KEY_AGENTPROPERTIES));
+    		 clientProperties =  (requestParams.getString(KEY_CLIENTPROPERTIES));
+    		 apiProperties = (requestParams.getString(KEY_APIPROPERTIES));
+      }
+    	 catch (JSONException e) {
+    		 validate = false;
+    		 throw new BadRequestException ("Invalid keys in the JSON Object.", e);
+    	 }
+    	 if (System.getenv(agentProperties) == null){
+    		 validate = false;
+    		 LOGGER.error("The environment variable does not point to a valid agent properties file.");
+    		 }
+    	 if (System.getenv(apiProperties) == null){
+    		 validate = false;
+    		 LOGGER.error("The environment variable does not point to a valid api properties file.");
+    		 }
+    	 if (System.getenv(clientProperties) == null){
+    		 validate = false;
+    		 LOGGER.error("The environment variable does not point to a valid client properties file.");
+    		 }
+    	 }
+	return validate;
+    }
+    
+ // TODO: Use proper argument parsing
     /**
      * Main method that runs through all steps to update the data received from the ThingsBoard API.
      * defined in the provided properties file.
      * @param args The command line arguments. Three properties files should be passed here in order: 1) input agent
      *             2) time series client 3) API connector.
      */
-    public static void main(String[] args) {
+    
+    public static void initializeAgent(String[] args) {
 
         // Ensure that there are three properties files
         if (args.length != 3) {
